c894e82b51e95d2eb302d025c710fa432215666a
Thai.Cuong <thaivancuong1993@gmail.com>
2021-09-05 10:53:38 +0700

    test change value

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
         <div>
             <ul>
                 <li>
-                    TRANG CHU
+                    TRANG CHU THAI CUONG
                 </li>
             </ul>
         </div>
